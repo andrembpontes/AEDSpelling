29d1c583f3b625bc4609dbf3d01dabef0a777910
andrembpontes <andrembpontes@gmail.com>
2014-10-01 16:54:46 +0100

    ADDED IWordOccurrence

--- eclipseProjects/AEDSpelling/src/aed/spelling/app/IWordOccurrence.java
+++ eclipseProjects/AEDSpelling/src/aed/spelling/app/IWordOccurrence.java
@@ -1,5 +1,36 @@
 package aed.spelling.app;
 
-public interface IWordOccurrence {
+import aed.dataStructures.Iterator;
 
+/**
+ * @author Andre Pontes (42845)
+ */
+public interface IWordOccurrence {
+	/**
+	 * Return times word appear on text
+	 * 
+	 * @return Times word appear on text
+	 */
+	int frequency();
+	
+	/**
+	 * Return word
+	 * 
+	 * @return Word
+	 */
+	String getWord();
+	
+	/**
+	 * Verify if word is in the dictionary
+	 * 
+	 * @return True if correct, False if not
+	 */
+	boolean isCorrect();
+	
+	/**
+	 * Lines number where word is present
+	 * 
+	 * @return Iterator for lines number where word is present
+	 */
+	Iterator<Integer> linesNr();
 }
