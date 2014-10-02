0d7d90e1c5ad685185ea2fd9f0266b20d59930b8
gbfm <gbfm@localhost.localdomain>
2014-10-02 18:51:33 +0100

    UPDATED IAnalysisResults

--- eclipseProjects/AEDSpelling/src/aed/spelling/app/IAnalysisResults.java
+++ eclipseProjects/AEDSpelling/src/aed/spelling/app/IAnalysisResults.java
@@ -5,10 +5,26 @@ import aed.dataStructures.Iterator;
 
 public interface IAnalysisResults {
 	
+	/**
+	 * Returns an iterator the errors found
+	 * 
+	 * @return An iterator the errors found
+	 */
 	Iterator<IWordOccurrence> errors();
 	
+	/**
+	 * Returns the frequency of a word
+	 * 
+	 * @param word Word to check the frequency for
+	 * @return The frequency of a word
+	 */
 	int frequency(String word);
 	
+	/**
+	 * Returns an iterator of occurrences of a word
+	 * 
+	 * @return An iterator of occurrences of a word
+	 */
 	Iterator<IWordOccurrence> occurrences();	
 }
 
