6844dd8839338138735af4c0563f38890e0bb13e
GoncaloBFM <goncalo.bfm@gmail.com>
2014-11-26 16:07:25 +0000

    UPDATED Comments

--- eclipseProjects/AEDSpelling/src/aed/spelling/LinesIterator.java
+++ eclipseProjects/AEDSpelling/src/aed/spelling/LinesIterator.java
@@ -1,32 +1,36 @@
-package aed.dataStructures;
+package aed.spelling;
+
+import aed.dataStructures.*;
+import aed.spelling.InvalidLineNumberException;
+import aed.spelling.InvalidLineRangeException;
 
 /**
  * @author Andre Pontes (42845) <am.pontes@campus.fct.unl.pt>
  * @author Goncalo Marcelino (43178) <gb.marcelino@campus.fct.unl.pt>
  * @param <E> Type of array elements
  */
-public class ArrayListIterator<E> implements Iterator<E> {
+class LinesIterator<E> implements Iterator<E> {
 
     private static final long	serialVersionUID	= 1L;
 
-    private ArrayList<E>		list;
+    private InsertionList<E>		list;
     private int					current, firstIndex, lastIndex;
 
-    public ArrayListIterator(ArrayList<E> list) throws InvalidIndexException, InvalidIndexRangeException{
+    protected LinesIterator(InsertionList<E> list) throws InvalidLineNumberException, InvalidLineRangeException {
         this(list, 0, list.size() - 1);
     }
 
-    public ArrayListIterator(ArrayList<E> list, int firstIndex) throws InvalidIndexException, InvalidIndexRangeException{
+    protected LinesIterator(InsertionList<E> list, int firstIndex) throws InvalidLineNumberException, InvalidLineRangeException {
         this(list, firstIndex, list.size() - 1);
     }
 
-    public ArrayListIterator(ArrayList<E> list, int firstIndex, int lastIndex) throws InvalidIndexException, InvalidIndexRangeException{
+    protected LinesIterator(InsertionList<E> list, int firstIndex, int lastIndex) throws InvalidLineNumberException, InvalidLineRangeException {
 
         if (!(firstIndex >= 0 && lastIndex < list.size()))
-            throw new InvalidIndexException();
+            throw new InvalidLineNumberException();
 
         if (lastIndex - firstIndex < 0)
-            throw new InvalidIndexRangeException();
+            throw new InvalidLineRangeException();
 
         this.list = list;
         this.firstIndex = firstIndex;
